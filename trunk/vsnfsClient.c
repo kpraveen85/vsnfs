f4bdba0cc469d58343be5e5834a8abedff1dc93d
findkb <findkb@1c9a59a8-d401-11de-901b-a378632c6355>
2009-11-25 03:31:42 +0000

    Just detaching server files for Prabhakar

--- trunk/vsnfsClient.c
+++ trunk/vsnfsClient.c
@@ -8,3 +8,25 @@
  *(C) Karthik Balaji <findkb@gmail.com>
  *
  */
+
+#include <linux/sched.h>
+#include <linux/time.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/string.h>
+#include <linux/stat.h>
+#include <linux/errno.h>
+#include <linux/unistd.h>
+#include <linux/sunrpc/clnt.h>
+#include <linux/sunrpc/stats.h>
+#include <linux/sunrpc/metrics.h>
+#include <linux/sunrpc/xprtsock.h>
+#include <linux/sunrpc/xprtrdma.h>
+#include <linux/vfs.h>
+#include <linux/inet.h>
+
+#include <asm/system.h>
+
+#include "vsnfsClient.h"
+#include "vsnfsMount.h"
+#include "vsnfs.h"
