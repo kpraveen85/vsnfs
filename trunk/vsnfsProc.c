0c99ee349f674cd473f380a3912f98b39bbc2276
findkb <findkb@1c9a59a8-d401-11de-901b-a378632c6355>
2009-12-03 07:07:12 +0000

    Won't compile. Just for Praveen or Prabhakar to fix up the mount procedure for client

--- trunk/vsnfsProc.c
+++ trunk/vsnfsProc.c
@@ -11,26 +11,48 @@
 
 #include <linux/types.h>
 #include <linux/param.h>
-#include <linux/slab.h>
-#include <linux/time.h>
 #include <linux/mm.h>
 #include <linux/errno.h>
 #include <linux/string.h>
 #include <linux/in.h>
 #include <linux/pagemap.h>
 #include <linux/sunrpc/clnt.h>
-#include <linux/nfs.h>
-#include <linux/nfs2.h>
-#include <linux/nfs_fs.h>
-#include <linux/nfs_page.h>
 #include <linux/lockd/bind.h>
+#include <linux/fs.h>
 #include "vsnfsClient.h"
+#include "vsnfsXdr.h"
+
+static int
+vsnfs_proc_readdir(struct dentry *dentry, u64 cookie, struct page *page,
+					unsigned int count, int plus)
+{
+	int status;
+	struct inode *dir = dentry->d_inode;
+	struct vsnfs_readdirargs arg = {
+		.fh		= VSNFS_FH(dir),
+		.cookie = cookie,
+		.count  = count,
+		.pages  = &page,
+	};
+	struct rpc_message msg = {
+		.rpc_proc	= &vsnfs_procedures[VSNFSPROC_READDIR],
+		.rpc_argp	= &arg,
+		.rpc_cred	= NULL,
+	};
+
+	printk("VSNFS call readdir %d\n", (unsigned int)cookie);
+	status = rpc_call_sync(VSNFS_CLIENT(dir), &msg, 0);
+
+	printk("NFS reply readdir: %d\n", status);
+	return status;
+}
 
 const struct vsnfs_rpc_ops vsnfs_clientops = {
 	.version		 = 1;
 	.dentry_op		 = &vsnfs_dentry_operations,
 	.dir_inode_ops	 = &vsnfs_dir_inode_operations,
 	.file_inode_ops	 = &vsnfs_file_inode_operations,
+	.getroot		 = vsnfs_proc_get_root,
 	.lookup			 = vsnfs_proc_lookup,
 	.create			 = vsnfs_proc_create,
 	.remove			 = vsnfs_proc_remove,
--- trunk/vsnfsXdr.h
+++ trunk/vsnfsXdr.h
@@ -8,17 +8,57 @@
  *(C) Karthik Balaji <findkb@gmail.com>
  *
  */
+#ifndef _VSNFSXDR_H
+#define _VSNFSXDR_H
 
+#include <linux/sunrpc/clnt.h>
+#include <linux/sunrpc/xdr.h>
+#include <linux/sunrpc/xprt.h>
+#include <linux/mm.h>
 #include "vsnfs.h"
 #include "vsnfsClient.h"
 
 /*
- * Arguements to read call (JUST A SAMPLE)
+ * Arguements to readdir call (JUST A SAMPLE)
  */
 
-struct vsnfs_readargs {
-	struct vsnfs_fh *fh;
-	__u64 offset;
-	__u64 count;
+struct vsnfs_readdirargs {
+	struct vsnfs_fh*	fh;
+	__u32				cookie;
+	unsigned int		count;
+	struct page**		pages;
 }
-	
+
+struct vsnfs_entry {
+	__u64				ino;
+	__u64				cookie,
+					prev_cookie;
+	const char *		name;
+	unsigned int		len;
+	int					eof;
+	struct vsnfs_fh *	fh;
+};	
+
+struct vsnfs_rpc_ops {
+	int		version;		/*Protocol Version*/
+	const struct dentry_operations *dentry_ops;
+	const struct inode_operations *dir_inode_ops;
+	const struct inode_operations *file_inode_ops;
+
+	int		(*getroot) (struct vsnfs_server *, struct vsnfs_fh *);
+	int		(*lookup)  (struct inode *, struct qstr *, struct vsnfs_fh *);
+	int		(*create)  (struct inode *, struct dentry *, struct iattr *,
+						int, struct vsnfs_fh *);
+	int		(*remove)  (struct dentry *, struct qstr *);
+	int		(*mkdir)   (struct inode *, struct dentry *, struct iattr *);
+	int		(*rmdir)   (struct inode *, struct qstr *);
+	int		(*readdir) (struct dentry *, u64, struct page *,
+						unsigned int, int);
+};
+
+extern const struct vsnfs_rpc_ops vsnfs_clientops;
+extern struct rpc_version vsnfs_version;
+extern struct rpc_program vsnfs_program;
+extern struct rpc_stat vsnfs_rpcstat;
+
+#endif
