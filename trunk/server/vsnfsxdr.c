d961bed368f2de9abbdd3b1ecae824f46c7ae874
kpraveen85 <kpraveen85@1c9a59a8-d401-11de-901b-a378632c6355>
2009-12-03 08:44:09 +0000

    properly compiling code

--- trunk/server/vsnfsxdr.c
+++ trunk/server/vsnfsxdr.c
@@ -1,10 +1,16 @@
 #include <linux/types.h>
 #include <linux/sunrpc/xdr.h>
 #include <linux/sunrpc/svc.h>
+#include "../vsnfs.h"
+#include "vsnfsd.h"
+#include "xdr.h"
+
 
 /*
  * XDR encode/decode helper functions
  */
+
+
 static struct svc_fh *
 fh_init(struct svc_fh *fhp, int maxsize)
 {
@@ -12,7 +18,7 @@ fh_init(struct svc_fh *fhp, int maxsize)
   return fhp;
 }
 
-static _be32 *
+static __be32 *
 decode_fh(__be32 *p,struct svc_fh *fhp)
 {
   fh_init(fhp,VSNFS_FHSIZE);
@@ -34,9 +40,16 @@ vsnfssvc_decode_void(struct svc_rqst *rqstp, __be32 *p, void *dummy)
 	return xdr_argsize_check(rqstp, p);
 }
 
+/*
 int 
-vsnfssvc_decode_readdirargs(struct svc_rqst *rqstp, __be32 *p, 
-                              struct vsnfsd_readdirargs *args)
+vsnfssvc_decode_readargs(struct svc_rqst *rqstp, __be32 *p, struct vsnfsd_readargs *args)
+{
+  unsigned int len;
+  
+}
+*/
+int 
+vsnfssvc_decode_readdirargs(struct svc_rqst *rqstp, __be32 *p, struct vsnfsd_readdirargs *args)
 {
   if(!(p=decode_fh(p,&args->fh)))
     return 0;
@@ -61,9 +74,16 @@ vsnfssvc_encode_void(struct svc_rqst *rqstp, __be32 *p, void *dummy)
 	return xdr_ressize_check(rqstp, p);
 }
 
+/*
 int 
-vsnfssvc_encode_readdirres(struct svc_rqst *rqstp, __be32 *p, 
-                                   struct vsnfsd_readdirres *resp)
+vsnfssvc_encode_readres(struct svc_rqst *rqstp, __be32 *p, struct vsnfsd_readres *resp)
+{
+
+}
+*/
+
+int 
+vsnfssvc_encode_readdirres(struct svc_rqst *rqstp, __be32 *p, struct vsnfsd_readdirres *resp)
 {
   xdr_ressize_check(rqstp,p);
   p=resp->buf;
@@ -72,3 +92,5 @@ vsnfssvc_encode_readdirres(struct svc_rqst *rqstp, __be32 *p,
   rqstp->rq_res.page_len=(((unsigned long)p-1) & ~PAGE_MASK)+1;
   return 1;
 }
+
+
